7d9aa7bc24f44751180bca520ad170147e48bb4e
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-09-27 15:31:37 -0300

    exercicio realizado

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello Wolrd");
+
+console.log("Meu nome é 'Gabriel'");
+
+console.log('Testanto aspas simples');
+console.log("Testanto aspas dupla");
+console.log("\n");
+console.log(`A soma de 16 e 6 é: ${16+6}`);
