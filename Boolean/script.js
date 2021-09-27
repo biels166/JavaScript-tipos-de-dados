b536cdbaa7eb848cb9f123d08cef1991c3240d52
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-09-27 16:09:26 -0300

    Update script.js

--- Boolean/script.js
+++ Boolean/script.js
@@ -6,13 +6,18 @@ console.log("\nComparações com <");
 console.log(5 < 10);
 console.log((30 % 2) < 5);
 
-console.log("Comparações com ==");
+console.log("\nComparações com ==");
 console.log("Gabriel" == "Gabriel");
 console.log((30 / 2) == 15);
 console.log("15" == 15);
 console.log(20 == 15);
 
-console.log("Comparações com ===");
+console.log("\nComparações com ===");
 console.log("Gabriel" === "Gabriel");
 console.log((30 / 2) === 15);
 console.log("15" === 15);
+
+console.log("\nComparações com !=");
+console.log("Gabriel" != "Gabriel");
+console.log((30 / 2) != 15);
+console.log(16 != 15);
