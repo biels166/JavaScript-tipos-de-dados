1d632b6e28a0c3b8cb7d077002e36587e48e5af3
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-09-27 15:47:43 -0300

    number index

--- Number/script.js
+++ Number/script.js
@@ -2,7 +2,7 @@ console.log("Imprimindo Números Inteiros:");
 console.log(16);
 console.log(16.5);
 
-console.log("\n Imprimindo Resultados Aritiméticos:");
+console.log("\nImprimindo Resultados Aritiméticos:");
 console.log(5 + 5);
 console.log(10 - 5);
 console.log(5 * 5);
