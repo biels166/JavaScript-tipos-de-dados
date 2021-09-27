c1573b1adc4beae101d12df1525902c56e3ea8d0
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-09-27 16:17:13 -0300

    update script

--- Boolean/script.js
+++ Boolean/script.js
@@ -21,3 +21,17 @@ console.log("\nComparações com !=");
 console.log("Gabriel" != "Gabriel");
 console.log((30 / 2) != 15);
 console.log(16 != 15);
+
+console.log("\nOperador AND (&&)");
+console.log(((1 + 1) == 2) && (5 > 1));
+console.log(true && false);
+
+console.log("\nOperador OR ||");
+console.log(((1 + 1) == 2) || (5 == 1));
+console.log(true || false);
+
+console.log("\nOperador NOT !");
+console.log(!(((1 + 1) == 2) || (5 == 1)));
+console.log(!(true || false));
+console.log(!(((1 + 1) == 2) && (5 > 1)));
+console.log(!(true && false));
