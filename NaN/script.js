0c16aabb4eb9477de787dcd8c700dd6083a7cb94
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-09-27 16:26:40 -0300

    Update script.js

--- NaN/script.js
+++ NaN/script.js
@@ -2,6 +2,6 @@ console.log("Testando a Correção Sinleciosa do JS");
 console.log("5" + 5);
 console.log(typeof ("5" + 5));
 
-console.log("Realizando somas entre number e string");
+console.log("\nRealizando somas entre number e string");
 console.log("Gabriel" * 5);
-console.log(typeof ("Gabriel" * 5));
+console.log(typeof ("Gabriel" + 5));
