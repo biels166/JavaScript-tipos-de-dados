9c044e429ebbc75dd5d5eb9b2e5a781f5266772c
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-09-27 15:44:47 -0300

    script numbers

---
+++ Number/script.js
@@ -0,0 +1,11 @@
+console.log("Imprimindo Números Inteiros:");
+console.log(16);
+console.log(16.5);
+
+console.log("\n Imprimindo Resultados Aritiméticos:");
+console.log(5 + 5);
+console.log(10 - 5);
+console.log(5 * 5);
+console.log(10 / 5);
+console.log(5 + 5);
+console.log(10000000 % 500);
